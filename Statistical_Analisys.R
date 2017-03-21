3485a2da08f223b5d8745e061fd98a876d2ab74d
Alberto Castillo Lamas <alcasla90@gmail.com>
2017-03-21 20:18:59 +0100

    Missing Values computation
    New function compute giving a dataset the number of NAs by feature

--- Statistical_Analisys.R
+++ Statistical_Analisys.R
@@ -1,5 +1,5 @@
 #Calcule percentage for each label/value
-#     feature - whatever feature from a dataset, specifically categoric features
+#     feature - whatever feature from a dataset, specifically categoric or few levels features
 ClassPercentages = function(feature){
   labels = unique(feature)
   cat('Percentages from labels:', labels, '\n')
@@ -8,4 +8,19 @@ ClassPercentages = function(feature){
 
 #Example
 ClassPercentages(math.tra$PV1MATH)    #percentage labels from class column = PV1MATH and dataset = math.tra
+apply(math.tra, MARGIN=2, ClassPercentages)     #apply function for each column
 
+
+#Calcule number of NAs (missing values) to each feature
+#     dataset - the full dataset
+#     return a number list of NAs with the same features order in the dataset
+numNA = function(dataset){
+  cat('Number of NAs by feature')
+  sapply(1:dim(dataset)[2], function(x){
+    cat(labels(dataset)[[2]][x], sum(which(is.na(dataset[,x]))), '\n')
+    return( sum(which(is.na(dataset[,x]))) )
+  } )
+}
+
+#Example
+numNA(math.tra)
